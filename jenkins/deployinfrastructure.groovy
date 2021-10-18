f12d99b6b866f025534d932c9180b0c701862d60
Артем Гусаров <kromelky@gmail.com>
2021-10-18 23:48:52 +0300

    Fixing pipeline errors

--- jenkins/deployinfrastructure.groovy
+++ jenkins/deployinfrastructure.groovy
@@ -35,7 +35,8 @@ pipeline {
             steps {
                 dir("terraform"){
                     withCredentials([usernamePassword(credentialsId: registryCredentials, passwordVariable: 'C_PASS', usernameVariable: 'C_USER')]) {
-                        sh "terraform plan -var-file=\'tfvars/dev.tfvars\' -var \'docker_pass=${C_PASS}\' -var \'docker_login=${C_USER}\' -auto-approve"
+                        echo "${C_PASS}"
+                        sh "terraform plan -var-file=\"tfvars/dev.tfvars\" -var \"docker_pass=${C_PASS}\" -var \"docker_login=${C_USER}\" -auto-approve"
                     }
                 }
             }
