e4697f316185ce356a49fef175019437e320286b
Артем Гусаров <kromelky@gmail.com>
2021-10-19 00:20:36 +0300

    Fixing pipeline errors

--- terraform/WebServers.tf
+++ terraform/WebServers.tf
@@ -8,18 +8,22 @@ resource "aws_instance" "webserver" {
     subnet_id  = aws_subnet.public[count.index].id
     associate_public_ip_address = true
     vpc_security_group_ids = [aws_security_group.sg_allow_web.id, aws_security_group.sg_allow_ssh.id]
-    user_data = templatefile("/template/WebServersInit.sh", {
-      imageName = "${var.docker_image_name}"
-      docker_login = "${var.docker_login}"
-      docker_pass = "${var.docker_pass}"
-    })
-  
-  tags = {
+    user_data = "${data.template_file.docker_templ.rendered}"
+    tags = {
       Name = "${var.environment} ${var.build_number} Web Server ${ count.index + 1}"
-  }
+    }
 
   lifecycle {
     create_before_destroy = true
+  }
+}
+
+data "template_file" "docker_templ" {
+  template = file("/template/WebServersInit.sh")
+  vars = {
+    imageName = var.docker_image_name
+    docker_login = var.docker_login
+    docker_pass = var.docker_pass
   }
 }
 
